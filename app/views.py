26b919e51e5a0b327e2c7e1f276ac92b99b55d4d
lrakoto <lova@threeohfivestudios.com>
2022-12-10 16:06:13 -0800

    added events directory to templates and pulled static data from views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 
+class Event:
+    def __init__(self, userId, name, date, location, description, image, capacity):
+        self.name = name
+        self.userId = userId
+        self.date = date
+        self.location = location
+        self.description = description
+        self.image = image
+        self.capacity = capacity
+
+events = [
+    Event(
+        1, 
+        'Event 1', 
+        'January', 
+        'Los Angeles', 
+        'this is a cool event', 
+        'https://unsplash.com/photos/QQMnCX3B07c',
+        200,
+    ),
+    Event(
+        1, 
+        'Event 2', 
+        'February', 
+        'Los Angeles', 
+        'this is a cool event #2', 
+        'https://unsplash.com/photos/QQMnCX3B07c',
+        200,
+    ),
+    
+]
+
 # Views
 def index(request):
   return render(request, 'index.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def eventspage(request):
+  return render(request, 'events/index.html', { 'events': events })
