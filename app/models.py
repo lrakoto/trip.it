c9f87ed8c7256218795a42691594af73aaca5bb7
lrakoto <lova@threeohfivestudios.com>
2022-12-10 16:47:50 -0800

    added migrations for Events

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    image = models.CharField(max_length=250)
+    capacity = models.IntegerField()
+    created = models.DateTimeField('date published')
+
+    def __str__(self):
+            return self.name, self.date, self.location, self.description, self.image
+
+    def was_published_recently(self):
+        return self.created >= timezone.now() - datetime.timedelta(days=1)
--- trip_it/settings.py
+++ trip_it/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'trip_it.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trip_it',
     }
 }
 
