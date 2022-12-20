41a9f1519bff832704f2c548b6726ca56e02a735
lrakoto <lova@threeohfivestudios.com>
2022-12-19 21:27:23 -0800

    added Google Maps API and map to index.html, used geolocate method to grab current location and display on map

--- app/views.py
+++ app/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, HttpResponse
 from trip_it import settings
 from .models import Event
+import googlemaps
+from json import *
 
 events = Event.objects.all()
 gkey = settings.GMAPS_KEY
 
+# Google maps API Key
+gmaps = googlemaps.Client(key=gkey)
+
+# retrieve coordinates for current location
+location = gmaps.geolocate()
+
 # Views
-
 def index(request):
-    return render(request, 'index.html', {'googlekey': gkey})
-
-def test(request):
-    return HttpResponse(f'HEY:{gkey}')
+    # assign google maps data to variable
+    mapdata = {
+        'mapdata': location
+    }
+    # convert data into JSON for use in browser
+    mapJSON = dumps(mapdata)
+    return render(request, 'index.html', {'location': location, 'googlekey': gkey, 'maps': mapJSON})
 
 def about(request):
     return render(request, 'about.html')
 
-
 def eventspage(request):
     return render(request, 'events/index.html', {'events': events})
-
 
 def eventdetails(request, event_id):
     event = Event.objects.get(id=event_id)
